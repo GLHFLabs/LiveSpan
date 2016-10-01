9b84a817b82b237bffcdcd9fb167574808cd8443
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2016-10-01 19:23:01 +0100

    First route.

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,12 @@ RUN apk add --no-cache libstdc++ python3 && \
     rm -r /usr/lib/python*/ensurepip && \
     pip3 install --upgrade pip setuptools && \
     rm -r /root/.cache
+RUN mkdir tmp
+EXPOSE 6000
 COPY requirements.txt .
 RUN pip3 install -r requirements.txt
 COPY api.py .
 COPY BalanceSpan/algo/BalanceSpan .
 COPY BalanceSpan/tests/example/binit .
 COPY BalanceSpan/tests/example/sc2replay_converter.py .
-ENTRYPOINT ["flask", "api.py"]
+ENTRYPOINT ["gunicorn", "-b 127.0.0.1:6000", "api:app"]
--- api.py
+++ api.py
@@ -1,8 +1,15 @@
 from flask import Flask, request
 from sc2replay_converter import main as convert
+import subprocess
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "/usr/local/app/tmp"
+
+subprocess.run("python sc2replay_converter.py replays/ 600 30")
 
 @app.route('/balance', methods = 'POST')
-def balance: pass
-#call to convert etc.
+def balance:
+    f = request.files["replay"]
+    filename = secure_filename(f.filename)
+    f.save("/usr/local/app/tmp/" + filename)
+    subprocess.run("./BalanceSpan ")
